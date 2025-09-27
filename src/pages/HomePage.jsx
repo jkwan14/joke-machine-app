409bf0546af40cc521b1d01d1ace538ad5344f89
Jonathan Kwan <jkwan14@gmail.com>
2025-09-27 14:40:03 -0400

    Added radio buttons for category selection

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,36 @@ export default function HomePage() {
   return (
     <div className="wallpaper-home">
       <h1>Welcome to the Joke Machine!</h1>
+
+      <form>
+        <fieldset>
+          <legend>Please select a category:</legend>
+          <label>
+            <input type="radio" name="category" value="Any" defaultChecked />
+            Any
+          </label>
+
+          <label>
+            <input type="radio" name="category" value="Programming" />
+            Programming
+          </label>
+
+          <label>
+            <input type="radio" name="category" value="Pun" />
+            Pun
+          </label>
+
+          <label>
+            <input type="radio" name="category" value="Halloween" />
+            Halloween
+          </label>
+
+          <label>
+            <input type="radio" name="category" value="Christmas" />
+            Christmas
+          </label>
+        </fieldset>
+      </form>
     </div>
   );
 }
